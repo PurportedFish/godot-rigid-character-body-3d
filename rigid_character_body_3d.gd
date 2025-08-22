b68e41a06461e912fe4919d55ddba3cb17c02d1c
PurportedFish <purportedfish@gmail.com>
2025-08-21 21:54:44 -0400

    Add constant speed on floors property and organized variables
    if floor_constant_speed is set to true, the body will attempt to reach
    the target_velocity regardless of how steep the slope is. Otherwise,
    the body will be slowed going up a slope.

--- rigid_character_body_3d.gd
+++ rigid_character_body_3d.gd
@@ -4,10 +4,13 @@ extends RigidBody3D
 
 @export var up_direction: Vector3 = Vector3.UP
 @export var neck_height: float = 1.5
-@export var knee_height: float = 0.5
-@export var ceiling_min_angle: float = deg_to_rad(105.0)
-@export var floor_max_angle: float = deg_to_rad(45.0)
-@export var slide_on_slope: bool = false
+@export_range(0.0, 180, 1.0, "radians_as_degrees") var ceiling_min_angle: float = deg_to_rad(105.0)
+@export_group("Floor", "floor")
+@export var floor_stop_on_slope: bool = true
+@export var floor_constant_speed: bool = false
+@export var floor_step_height: float = 0.5
+@export_range(0.0, 180, 1.0, "radians_as_degrees") var floor_max_angle: float = deg_to_rad(45.0)
+
 
 var target_velocity: Vector3 = Vector3.ZERO
 var acceleration_magnitude: float = 10.0
@@ -31,7 +34,7 @@ func _ready() -> void:
 
 
 func move_and_slide() -> void:
-	_detect_ceiling_floor_wall()
+	var floor_normal: Vector3 = _detect_ceiling_floor_wall()
 	
 	if not target_velocity:
 		return
@@ -39,51 +42,14 @@ func move_and_slide() -> void:
 	var move_magnitude: float = mass * acceleration_magnitude
 	var move_force: Vector3 = move_magnitude * target_velocity.normalized()
 	
+	if is_on_floor():
+		if not floor_constant_speed and  target_velocity.normalized().dot(floor_normal) < -0.1:
+			move_force *= clampf(floor_normal.dot(up_direction), -1.0, 1.0)
+	
 	var drag_scale: float = linear_velocity.length() / target_velocity.length()
 	var drag_force: Vector3 = move_magnitude * drag_scale * -Vector3(linear_velocity.x, 0.0, linear_velocity.z).normalized()
 	
 	apply_force(move_force + drag_force)
-
-
-func _detect_ceiling_floor_wall() -> void:
-	_state = PhysicsServer3D.body_get_direct_state(get_rid())
-	
-	_is_on_ceiling = false
-	_is_on_floor = false
-	_is_on_wall = false
-	
-	for i in _state.get_contact_count():
-		var contact_position: Vector3 = to_local(_state.get_contact_collider_position(i))
-		var normal: Vector3 = _state.get_contact_local_normal(i)
-		var contact_angle: float = acos(normal.dot(up_direction))
-		
-		if (
-			contact_position.y > knee_height 
-			and contact_position.y < neck_height
-			and contact_angle > floor_max_angle
-			and contact_angle < ceiling_min_angle
-		):
-			_is_on_wall = true
-			continue
-		
-		if (
-			contact_position.y >= neck_height 
-			and (
-				contact_angle >= ceiling_min_angle 
-				or is_equal_approx(contact_angle, ceiling_min_angle)
-			)
-		):
-			_is_on_ceiling = true
-			# TODO: Add ceiling behavior
-			continue
-		
-		_is_on_floor = true
-		
-		if slide_on_slope:
-			continue
-		
-		if contact_angle <= floor_max_angle or is_equal_approx(contact_angle, floor_max_angle):
-			apply_central_force(-get_gravity().length() * mass * Vector3.DOWN.slide(normal))
 
 
 func is_on_ceiling() -> bool:
@@ -108,3 +74,50 @@ func is_on_wall() -> bool:
 
 func is_on_wall_only() -> bool:
 	return _is_on_wall and not (_is_on_ceiling or _is_on_wall)
+
+
+func _detect_ceiling_floor_wall() -> Vector3:
+	var highest_floor_normal: Vector3 = Vector3.ZERO
+	_state = PhysicsServer3D.body_get_direct_state(get_rid())
+	
+	_is_on_ceiling = false
+	_is_on_floor = false
+	_is_on_wall = false
+	
+	for i in _state.get_contact_count():
+		var contact_position: Vector3 = to_local(_state.get_contact_collider_position(i))
+		var normal: Vector3 = _state.get_contact_local_normal(i)
+		var contact_angle: float = acos(normal.dot(up_direction))
+		
+		if (
+			contact_position.y > floor_step_height 
+			and contact_position.y < neck_height
+			and contact_angle > floor_max_angle
+			and contact_angle < ceiling_min_angle
+		):
+			_is_on_wall = true
+			continue
+		
+		if (
+			contact_position.y >= neck_height 
+			and (
+				contact_angle >= ceiling_min_angle 
+				or is_equal_approx(contact_angle, ceiling_min_angle)
+			)
+		):
+			_is_on_ceiling = true
+			# TODO: Add ceiling behavior
+			continue
+		
+		_is_on_floor = true
+		
+		if not floor_stop_on_slope:
+			continue
+		
+		if normal.y > highest_floor_normal.y:
+			highest_floor_normal = normal
+		
+		if contact_angle <= floor_max_angle or is_equal_approx(contact_angle, floor_max_angle):
+			apply_central_force(-get_gravity().length() * mass * Vector3.DOWN.slide(normal))
+	
+	return highest_floor_normal
